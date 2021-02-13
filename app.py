1b0faf6a6e1b4f60a20c6d241cd9be04f80d3248
cnulenka <sinulenka001@gmail.com>
2021-02-13 22:40:22 +0530

    added relationship between models

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref="venue", lazy=True)
 
 class Artist(db.Model):
     __tablename__ = 'artists'
@@ -56,6 +57,16 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref="artist", lazy=True)
+
+class Show(db.Model):
+  __tablename__ = 'shows'
+  
+  venue_id = db.Column(db.Integer, db.ForeignKey("venues.id"),
+                      primary_key=True)
+  artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"),
+                        primary_key=True)
+  start_time = db.Column(db.DateTime, nullable=False)
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
