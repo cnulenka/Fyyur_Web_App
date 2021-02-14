5421a8ae6b95e22f956119549bc1a2c19da04371
cnulenka <sinulenka001@gmail.com>
2021-02-14 13:12:03 +0530

    added new columns and default values

--- app.py
+++ app.py
@@ -34,20 +34,28 @@ migrate = Migrate(app,db)
 
 class Venue(db.Model):
     __tablename__ = 'venues'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500), default = "")
+    image_link = db.Column(db.String(500),
+                default = "https://images.unsplash.com/\
+                photo-1507901747481-84a4f64fda6d?ixid=\
+                MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB\
+                8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=\
+                crop&w=1050&q=80")
     facebook_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default = False)
-    seeking_description = db.Column(db.String(500), default = "")
     genres = db.Column(db.String(120))
     artists = db.relationship('Artist', secondary='shows',
               backref=db.backref('venues', lazy=True))
+    seeking_description = db.Column(db.String(500),
+                          default = "We are on the lookout \
+                            for a local artist to play every \
+                            two weeks. Please call us.")
+    website = db.Column(db.String(120), default = "")
 
 class Artist(db.Model):
     __tablename__ = 'artists'
@@ -58,10 +66,16 @@ class Artist(db.Model):
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500), default = "")
+    seeking_description = db.Column(db.String(500), default = "Looking for \
+                         shows to perform at in the San Francisco Bay Area!")
+    image_link = db.Column(db.String(500),
+                      default = "https://images.unsplash.com/photo-1526218\
+                      626217-dc65a29bb444?ixid=MXwxMjA3fDB8MHxwaG90by1wYWd\
+                      lfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=\
+                      crop&w=334&q=80")
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, default = False)
-    seeking_description = db.Column(db.String(500), default = "")
+    website = db.Column(db.String(120), default = "")
 
 class Show(db.Model):
   __tablename__ = 'shows'
